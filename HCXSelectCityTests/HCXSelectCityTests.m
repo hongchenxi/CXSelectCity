5adf4b55ead505831ce2c64a0c0ca3149fad15f5
hongchenxi <hongchenxi86@gmail.com>
2016-01-22 14:14:46 +0800

    Initial Commit

---
+++ HCXSelectCityTests/HCXSelectCityTests.m
@@ -0,0 +1,39 @@
+//
+//  HCXSelectCityTests.m
+//  HCXSelectCityTests
+//
+//  Created by 洪晨希 on 16/1/22.
+//  Copyright © 2016年 洪晨希. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HCXSelectCityTests : XCTestCase
+
+@end
+
+@implementation HCXSelectCityTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
